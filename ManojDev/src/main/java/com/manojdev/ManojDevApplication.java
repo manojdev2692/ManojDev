d92cb474a8ac15dbb4e4786c5410d48dd533ace7
manojdev2692 <manojdev2692@gmail.com>
2025-02-20 10:40:50 +0530

    create spring boot app

---
+++ ManojDev/src/main/java/com/manojdev/ManojDevApplication.java
@@ -0,0 +1,13 @@
+package com.manojdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManojDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManojDevApplication.class, args);
+    }
+
+}
---
+++ ManojDev/src/test/java/com/manojdev/ManojDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manojdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManojDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
